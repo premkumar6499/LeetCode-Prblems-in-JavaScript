9249d3e02c6e7e2811cea9a108343f0e8f499a3b
premkumar6499 <premjiguru_mca27@mepcoeng.ac.in>
2026-03-08 18:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (84.04%) - LeetSync

---
+++ 48-rotate-image/rotate-image.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    const n = matrix.length;
+
+    // Step 1: Transpose (Swap matrix[i][j] with matrix[j][i])
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    // Step 2: Reverse each row
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+};
