06fd63db8e53a08ac314378ab02ed2e979996cd7
premkumar6499 <premjiguru_mca27@mepcoeng.ac.in>
2026-03-10 09:42:44 +0530

    Time: 43 ms (70.01%) | Memory: 53.7 MB (79.43%) - LeetSync

---
+++ 2797-event-emitter/event-emitter.js
@@ -0,0 +1,45 @@
+class EventEmitter {
+    constructor() {
+        // Map of eventName -> Array of callback functions
+        this.events = new Map();
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+        if (!this.events.has(eventName)) {
+            this.events.set(eventName, []);
+        }
+
+        const listeners = this.events.get(eventName);
+        listeners.push(callback);
+
+        return {
+            unsubscribe: () => {
+                const index = listeners.indexOf(callback);
+                if (index !== -1) {
+                    listeners.splice(index, 1);
+                }
+                return undefined;
+            }
+        };
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        if (!this.events.has(eventName)) {
+            return [];
+        }
+
+        const listeners = this.events.get(eventName);
+        // Map over listeners to execute them and collect return values
+        return listeners.map(callback => callback(...args));
+    }
+}
